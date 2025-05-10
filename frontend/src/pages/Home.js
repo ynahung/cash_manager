022496d30775d7958c7e45aa28a2d092c6766706
alex <alexhungyn@gmail.com>
2025-05-10 20:43:20 +0800

    fix: remove unused Link import from Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography, Button, Link } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
